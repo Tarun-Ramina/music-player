1c4a6afff1593a1e5c8aa5dddee4292a625df12a
Tarun-Ramina <tarunramina@gmail.com>
2023-04-25 21:19:27 +0530

    first commit

---
+++ js/playlist.js
@@ -0,0 +1,62 @@
+let songs = [
+    {
+      name: "Aankhon Mein Teri",
+      artist: "KK",
+      img: "img1",
+      audio: "music1"
+    },
+    {
+      name: "Chand Sifarish",
+      artist: "Kailash Kher and Shaan",
+      img: "img2",
+      audio: "music2"
+    },
+    {
+      name: "Nachde Ne Saare",
+      artist: "Harshdeep Kaur, Jasleen Royal, and Siddharth Mahadevan",
+      img: "img3",
+      audio: "music3"
+    },
+    {
+      name: "Dekhte Dekhte",
+      artist: "Atif Aslam",
+      img: "img4",
+      audio: "music4"
+    },
+    {
+        name: "Falak Tak",
+        artist: "Mahalakshmi Iyer and Udit Narayan",
+        img: "img5",
+        audio: "music5"
+    },
+    {
+        name: "Kashmir Main Tu Kanyakumari",
+        artist: "Arijit Singh, Neeti Mohan, and Sunidhi Chauhan",
+        img: "img6",
+        audio: "music6"
+    },
+    {
+        name: "Tum Se Hi",
+        artist: "Mohit Chauhan",
+        img: "img9",
+        audio: "music9"
+    },
+    {
+        name: "Khuda Jaane Ke",
+        artist:"KK and Shilpa Rao",
+        img: "img7",
+        audio:"music7"
+    },
+    {
+        name: "Proper Patola",
+        artist:"Diljit Dosanjh",
+        img: "img10",
+        audio:"music10"
+    },
+    {
+        name: "Neele Neele Ambar Par",
+        artist:"Kishore Kumar",
+        img: "img8",
+        audio:"music8"
+    },
+]
